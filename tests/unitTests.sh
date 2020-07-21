9dcc905a12c74c55448bf2fd6aec87b4d1bfb052
Osama <osama21@aucegypt.edu>
2020-07-21 02:48:32 +0200

    added parallel execution

--- tests/unitTests.sh
+++ tests/unitTests.sh
@@ -6,27 +6,17 @@ export PYTHONPATH=$BASE_DIR/../build/src/swig/python:$PYTHONPATH
 
 echo "Running tests .."
 echo ""
-files=$(find $BASE_DIR/unitTestsPython -name "Test*.py")
-for file in $files
-do
-    name=$(echo $file | awk -F"/" '{print $NF}')
-    echo "$name"
-    echo ""
-    python3 $BASE_DIR/unitTestsPython/$name
-    echo ""
-done
-
-# echo "Running tests .."
-# echo "================"
-
-# echo "[1] dbNet Unit Test"
-# python3 $BASE_DIR/unitTestsPython/TestNet.py
-# echo ""
-
-# echo "[2] dbInst Unit Test"
-# python3 $BASE_DIR/unitTestsPython/TestInst.py
-# echo ""
-
-# echo "[5] Destroy Integration Test"
-# python3 $BASE_DIR/unitTestsPython/TestDestroy.py
-# echo ""
+if [[ $1 = "parallel" ]]
+then
+    unittest-parallel -s $BASE_DIR/unitTestsPython/ -p 'Test*.py' -q
+else
+    files=$(find $BASE_DIR/unitTestsPython -name "Test*.py")
+    for file in $files
+    do
+        name=$(echo $file | awk -F"/" '{print $NF}')
+        echo "$name"
+        echo ""
+        python3 $BASE_DIR/unitTestsPython/$name
+        echo ""
+    done
+fi

fcfcccfbc41ca457f7cb236ed54f9210892c4a55
Matt Liberty <matt.liberty@gmail.com>
2020-12-01 15:55:55 -0800

    fix unit test namespace

--- tests/cpp/TestLogger.cpp
+++ tests/cpp/TestLogger.cpp
@@ -15,15 +15,15 @@ BOOST_AUTO_TEST_CASE(test_default)
             sink_path = "/OpenDB/build/"+sink_path;//in Docker
     }
     ord::init();
-    BOOST_ASSERT(ordlog::info(ordlog::OPENDB,1,"Fresh start")==0);
-    BOOST_ASSERT(ordlog::addSinkFile(sink_path.c_str())==0);
-    BOOST_ASSERT(ordlog::warn(ordlog::OPENDB,2,"Added Sink")==0);
-    BOOST_ASSERT(ordlog::addSinkFile(sink_path.c_str())==-2);
-    BOOST_ASSERT(ordlog::removeSinkFile(sink_path.c_str())==0);
-    BOOST_ASSERT(ordlog::crit(ordlog::OPENDB,3,"Removed Sink")==0);
-    BOOST_ASSERT(ordlog::error(ordlog::OPENDB,10000,"Invalid Id")==-1);
-    BOOST_ASSERT(ordlog::error(ordlog::OPENDB,9999,"valid error Id")==0);
-    BOOST_ASSERT(ordlog::removeSinkFile(sink_path.c_str())==-2);
+    BOOST_ASSERT(ord::info(ord::OPENDB,1,"Fresh start")==0);
+    BOOST_ASSERT(ord::addSinkFile(sink_path.c_str())==0);
+    BOOST_ASSERT(ord::warn(ord::OPENDB,2,"Added Sink")==0);
+    BOOST_ASSERT(ord::addSinkFile(sink_path.c_str())==-2);
+    BOOST_ASSERT(ord::removeSinkFile(sink_path.c_str())==0);
+    BOOST_ASSERT(ord::crit(ord::OPENDB,3,"Removed Sink")==0);
+    BOOST_ASSERT(ord::error(ord::OPENDB,10000,"Invalid Id")==-1);
+    BOOST_ASSERT(ord::error(ord::OPENDB,9999,"valid error Id")==0);
+    BOOST_ASSERT(ord::removeSinkFile(sink_path.c_str())==-2);
 }
 BOOST_AUTO_TEST_CASE(test_init_file)
 {
@@ -34,10 +34,10 @@ BOOST_AUTO_TEST_CASE(test_init_file)
             sink_path = "/OpenDB/build/"+sink_path;//in Docker
     }
     ord::init(sink_path.c_str());
-    BOOST_ASSERT(ordlog::info(ordlog::OPENDB,1,"Fresh start in file")==0);
-    BOOST_ASSERT(ordlog::addSinkStdout()==0);
-    BOOST_ASSERT(ordlog::info(ordlog::OPENDB,2,"Added stdout")==0);
-    BOOST_ASSERT(ordlog::removeSinkStdout()==0);
-    BOOST_ASSERT(ordlog::info(ordlog::OPENDB,3,"Removed stdout")==0);
+    BOOST_ASSERT(ord::info(ord::OPENDB,1,"Fresh start in file")==0);
+    BOOST_ASSERT(ord::addSinkStdout()==0);
+    BOOST_ASSERT(ord::info(ord::OPENDB,2,"Added stdout")==0);
+    BOOST_ASSERT(ord::removeSinkStdout()==0);
+    BOOST_ASSERT(ord::info(ord::OPENDB,3,"Removed stdout")==0);
 }
 BOOST_AUTO_TEST_SUITE_END()
